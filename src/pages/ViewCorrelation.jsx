836985c5763428be9b0547c6bb8b4067e49b7087
andrewwatson126 <andrewwatason126@gmail.com>
2021-12-26 23:03:07 +0200

    save changes

--- src/pages/ViewCorrelation.jsx
+++ src/pages/ViewCorrelation.jsx
@@ -32,25 +32,67 @@ const ViewCorrelation = props => {
     });
 
 
-    /*
-    axios.get('http://apiserver:8000/projects/correlation/' + projectId)
-      .then(res => {
-        let result = (res && res.data && res.data[0].file) || [];
-        setCorrelation(res.data[0])
-      });
-    */
-
     axios({
       method: 'get',
-      url: 'http://apiserver:8000/projects/correlation/' + projectId
-      //responseType: 'stream'
+      url: 'http://apiserver:8000/projects/correlation/' + projectId,
+      responseType: 'blob'
     })
       .then(function (res) {
-        console.log(res)
-      }
-      )
+        //const base64 = btoa(new Uint8Array(res.data).reduce((data, byte) => data + String.fromCharCode(byte), ''))
+        //const base64 = Buffer.from(res.data, "binary").toString("base64")
+        //const base64 = btoa(res.data)
+        //const myarray = new Uint8Array(res.data);
+        //const myarray = res.data.arrayBuffer();
+        //console.log("myarray=", myarray)
+        //const base64 = btoa(uint8ToString(myarray));
+        //const base64 = await convertBlobtoBase64(res.data)
+        const d = toBinary(res.data.arrayBuffer)
+        console.log("d=", d);
+        const base64 = btoa(d)
+        //const base64 = btoa(res.data.Uint8Array)
+        console.log("res.data.arrayBuffer=", res.data.arrayBuffer);
+        console.log("res.data=", res.data);
+        console.log("base64=", base64);
+        setCorrelation("data:image/png;base64," + " " + base64);
+        //setCorrelation("data:image/png;base64," + " " + "iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==")
+      })
+
 
   }, [project.id]);
+
+  function toBinary(string) {
+    const codeUnits = new Uint16Array(string.length);
+    for (let i = 0; i < codeUnits.length; i++) {
+      codeUnits[i] = string.charCodeAt(i);
+    }
+    const charCodes = new Uint8Array(codeUnits.buffer);
+    let result = '';
+    for (let i = 0; i < charCodes.byteLength; i++) {
+      result += String.fromCharCode(charCodes[i]);
+    }
+    return result;
+  }
+
+  const uint8ToString = (buf) => {
+    var i, length, out = '';
+    for (i = 0, length = buf.length; i < length; i += 1) {
+      out += String.fromCharCode(buf[i]);
+    }
+    console.log("buf=", buf)
+    console.log("out=", out)
+    return out;
+  }
+
+  const convertBlobtoBase64 = async (blob) => {
+    return await blobtoBase64(blob);
+  }
+
+  const blobtoBase64 = blob => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsBinaryString(blob);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
 
 
   const displayRow = (algorithm, accuracy) => {
@@ -83,7 +125,8 @@ const ViewCorrelation = props => {
 
               CORRELATION
 
-              <img id="correlation" src="correlation.png" />
+              {console.log("correlation=", correlation)}
+              <img src={correlation} />
 
             </Paper>
           </Grid>
